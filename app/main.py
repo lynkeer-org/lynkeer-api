8f67cbad77cd5b43fa6ca9f879d30ab419318cab
Andres Gonzalez <afgonzalezr9003@gmail.com>
2025-04-17 11:41:10 -0500

    commit despues de instalar ruff y black  formater, limpiando algunos imports

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
-
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 from api.v1.routers import customers  # This should work if structure is correct
 
 from models import Transaction, Invoice
-from db import SessionDep, create_all_tables
-from sqlmodel import select
+from db import create_all_tables
 
 
-app = FastAPI(lifespan=create_all_tables) # This is a FastAPI application instance. The lifespan parameter is used to create and drop the database tables when the application starts and stops.
+app = FastAPI(
+    lifespan=create_all_tables
+)  # This is a FastAPI application instance. The lifespan parameter is used to create and drop the database tables when the application starts and stops.
 app.include_router(customers.router)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
 
 
-
-
-@app.post("/transactions") 
+@app.post("/transactions")
 async def create_transaction(transaction_data: Transaction):
-    
+
     return transaction_data
 
-@app.post("/invoices") 
+
+@app.post("/invoices")
 async def create_invoice(invoice_data: Invoice):
-    
+
     return invoice_data
