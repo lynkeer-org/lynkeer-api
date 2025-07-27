98469565452c538fa268c4024736eb80e0b8e04b
Andres Gonzalez <afgonzalezr9003@gmail.com>
2025-07-27 17:49:27 -0500

    protected the endpoints with the token

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from app.api.v1.endpoints import (
     auth,
@@ -10,17 +10,24 @@ from app.api.v1.endpoints import (
 from app import models
 
 from app.core.db import create_all_tables
+from app.core.security import get_current_user
 
 
 app = FastAPI(
     lifespan=create_all_tables
 )  # This is a FastAPI application instance. The lifespan parameter is used to create and drop the database tables when the application starts and stops.
 
-app.include_router(owner.router)  # This should work if structure is correct
-app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
-app.include_router(pass_model.router)
-app.include_router(pass_type.router)
-app.include_router(pass_field.router)
+app.include_router(
+    owner.router, dependencies=[Depends(get_current_user)]
+)  # This should work if structure is correct
+app.include_router(
+    auth.router,
+    prefix="/api/v1/auth",
+    tags=["Authentication"],
+)
+app.include_router(pass_model.router, dependencies=[Depends(get_current_user)])
+app.include_router(pass_type.router, dependencies=[Depends(get_current_user)])
+app.include_router(pass_field.router, dependencies=[Depends(get_current_user)])
 
 
 @app.get("/")
