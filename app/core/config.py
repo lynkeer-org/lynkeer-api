82884f3bf14cbeb1f08fcba8aba0dfe31f141571
Andres Gonzalez <afgonzalezr9003@gmail.com>
2025-08-08 18:59:35 -0500

    Time expiration token changed

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 settings = Settings()
