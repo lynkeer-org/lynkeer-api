5855c167836f6c319e08619740e35b725565aa72
Andrés González <afgonzalezr9003@gmail.com>
2025-08-30 08:44:25 -0500

    test_login_owner

--- app/tests/tests_owner.py
+++ app/tests/tests_owner.py
@@ -13,3 +13,14 @@ def test_create_owner(client):
         },
     )
     assert response.status_code == status.HTTP_201_CREATED
+
+def test_login_owner(client):
+    test_create_owner(client)  # Ensure owner exists before login
+    response = client.post(
+        "/api/v1/sign-in",
+        json={
+            "email": "andres@example.com",
+            "password": "securepassword",
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
